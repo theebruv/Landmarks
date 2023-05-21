162a773742969b0a0efee44c75a80b2d4da10b5f
Rodgers Tanui <rtankip@gmail.com>
2023-05-21 16:09:58 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Rodgers Tanui on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
