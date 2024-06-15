d46ed34bf8edcb841a88297744fe4d0fcf0aec24
Rodgers Tanui <iamkipchirchir@gmail.com>
2024-06-15 15:54:06 +0300

    Updated the model and added favorites feature

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -9,16 +9,17 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
+    var isFavorite: Bool
     
-    private var imagName: String
+    private var imageName: String
     var image: Image {
-        Image(imagName)
+        Image(imageName)
     }
     
     private var coordinates: Coordinates
